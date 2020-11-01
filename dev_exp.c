bac88d26051d16bf8a4326183258647979e34da8
KNick02 <70335914+KNick02@users.noreply.github.com>
2020-11-01 16:07:20 +0300

    Update dev_exp.c

--- dev_exp.c
+++ dev_exp.c
@@ -8,8 +8,6 @@ double dev_exp(double *radioactivity, double *time, int M, double decay_time)
         int i = 0;
         for (i = 0; i < M; i++)
             sum_residuals1 += pow(radioactivity[i] - expl(-time[i]/decay_time), 2);
-	
-	double res = sqrt(sum_residuals1)/M;
 
-	return res;
+	return sqrt(sum_residuals1)/M;
 }
